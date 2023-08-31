12bf26dd5b66f140dc76649c7fd3b5403f4bc23f
FreshFish <98007265+Freshman1001@users.noreply.github.com>
2023-08-31 08:52:44 +0800

    update

--- resnet101/feature_extractor.py
+++ resnet101/feature_extractor.py
@@ -5,12 +5,11 @@ import torchvision.transforms as transforms
 from torchvision import models
 from PIL import Image
 
-# 设置文件夹路径和模型
-folder_path = "test_img//people"
+folder_path = "images//no_defective"
 model = models.resnet101(pretrained=True)
 model.eval()
 
-# 图像预处理
+# 对于无缺陷图像，采用中心裁切进行预处理
 preprocess = transforms.Compose([
     transforms.Resize(256),
     transforms.CenterCrop(224),
@@ -19,34 +18,30 @@ preprocess = transforms.Compose([
                          std=[0.229, 0.224, 0.225]),
 ])
 
-# 用于存储特征的列表
+
 features_list = []
 
-# 遍历文件夹中的图像文件
 for filename in os.listdir(folder_path):
     if filename.endswith(".jpg") or filename.endswith(".png"):
         image_path = os.path.join(folder_path, filename)
 
-        # 加载图像并进行预处理
         image = Image.open(image_path).convert("RGB")
         image_tensor = preprocess(image)
         image_tensor = torch.unsqueeze(image_tensor, 0)
 
-        # 使用ResNet-101提取特征
         with torch.no_grad():
             features = model(image_tensor)
 
-        # 将特征转换为NumPy数组并添加到列表中
         features_np = features.numpy()
         features_list.append(features_np)
 
         print(f"Processed image: {filename}")
 
-# 将特征列表转换为NumPy数组
+
 all_features = np.concatenate(features_list, axis=0)
 
-# 保存特征数组为NumPy文件
-output_file_path = "resnet101//features.npy"
+
+output_file_path = "resnet101//no_defective_features.npy"
 np.save(output_file_path, all_features)
 
 print("特征保存完成")
--- t_SNE/image_feature_t-sne.py
+++ t_SNE/image_feature_t-sne.py
@@ -6,20 +6,19 @@ from sklearn.manifold import TSNE
 people_features = np.load("resnet101//people_features.npy")
 scene_features = np.load("resnet101//scene_features.npy")
 
-# 合并两个特征数组
+
 all_features = np.concatenate([people_features, scene_features], axis=0)
 
-# 创建对应于特征类别的标签
+
 num_people_samples = people_features.shape[0]
 num_scene_samples = scene_features.shape[0]
 labels = np.array(["people features"] * num_people_samples +
                   ["scene features"] * num_scene_samples)
 
-# 使用 t-SNE 进行降维和可视化
+
 tsne = TSNE(n_components=2, perplexity=1, random_state=42)
 embedded_features = tsne.fit_transform(all_features)
 
-# 根据标签分割嵌入特征
 people_embedded = embedded_features[:num_people_samples]
 scene_embedded = embedded_features[num_people_samples:]
 
